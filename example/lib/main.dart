833ef6692fe01fc381fa60edf3ab934a5fece2c8
tunglv <tunglv@softdreams.vn>
2025-01-11 16:27:52 +0700

    update gradle version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,11 @@
-import 'dart:typed_data';
-
-import 'package:flutter/material.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_sunmi_printer_plus/column_maker.dart';
+import 'package:flutter_sunmi_printer_plus/enums.dart';
 import 'package:flutter_sunmi_printer_plus/flutter_sunmi_printer_plus.dart';
 import 'package:flutter_sunmi_printer_plus/sunmi_style.dart';
-import 'package:flutter_sunmi_printer_plus/enums.dart';
-import 'package:flutter_sunmi_printer_plus/column_maker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -74,70 +72,66 @@ class _MyAppState extends State<MyApp> {
                   ElevatedButton(
                     child: const Text("print Text"),
                     onPressed: () async {
-                      try{
-                      await SunmiPrinter.printText(
-                          content: "Test String",
-                          style: SunmiStyle(
-                              fontSize: 20,
-                              isUnderLine: true,
-                              bold: false,
-                              align: SunmiPrintAlign.LEFT));
-                      await SunmiPrinter.lineWrap(1);
-                      await SunmiPrinter.printText(
-                          content: "Test String #2",
-                          style: SunmiStyle(
-                              fontSize: 24,
-                              isUnderLine: false,
-                              bold: true,
-                              align: SunmiPrintAlign.CENTER));
-                    await SunmiPrinter.feedPaper();
-                      }catch(err){
-                           errorMessage = err.toString();
-                           setState(() {});
+                      try {
+                        await SunmiPrinter.printText(
+                            content: "Test String",
+                            style: SunmiStyle(
+                                fontSize: 20,
+                                isUnderLine: true,
+                                bold: false,
+                                align: SunmiPrintAlign.LEFT));
+                        await SunmiPrinter.lineWrap(1);
+                        await SunmiPrinter.printText(
+                            content: "Test String #2",
+                            style: SunmiStyle(
+                                fontSize: 24,
+                                isUnderLine: false,
+                                bold: true,
+                                align: SunmiPrintAlign.CENTER));
+                        await SunmiPrinter.feedPaper();
+                      } catch (err) {
+                        errorMessage = err.toString();
+                        setState(() {});
                       }
                     },
                   ),
-                  
-                         ElevatedButton(
+                  ElevatedButton(
                     child: const Text("الطباعة بالعربي"),
                     onPressed: () async {
-                      try{
-                     
-                              await SunmiPrinter.printText(
-                          content: "نص تجريبي",
-                          style: SunmiStyle(
-                              fontSize: 24,
-                              isUnderLine: false,
-                              bold: true,
-                              align: SunmiPrintAlign.RIGHT));
-                await SunmiPrinter.lineWrap(1);
+                      try {
                         await SunmiPrinter.printText(
-                          content: "نص تجريبي",
-                          style: SunmiStyle(
-                              fontSize: 24,
-                              isUnderLine: false,
-                              bold: true,
-                              align: SunmiPrintAlign.CENTER));
-                await SunmiPrinter.lineWrap(1);
+                            content: "نص تجريبي",
+                            style: SunmiStyle(
+                                fontSize: 24,
+                                isUnderLine: false,
+                                bold: true,
+                                align: SunmiPrintAlign.RIGHT));
+                        await SunmiPrinter.lineWrap(1);
                         await SunmiPrinter.printText(
-                          content: "نص تجريبي",
-                          style: SunmiStyle(
-                              fontSize: 24,
-                              isUnderLine: false,
-                              bold: true,
-                              align: SunmiPrintAlign.LEFT));
-                await SunmiPrinter.lineWrap(1);
+                            content: "نص تجريبي",
+                            style: SunmiStyle(
+                                fontSize: 24,
+                                isUnderLine: false,
+                                bold: true,
+                                align: SunmiPrintAlign.CENTER));
+                        await SunmiPrinter.lineWrap(1);
+                        await SunmiPrinter.printText(
+                            content: "نص تجريبي",
+                            style: SunmiStyle(
+                                fontSize: 24,
+                                isUnderLine: false,
+                                bold: true,
+                                align: SunmiPrintAlign.LEFT));
+                        await SunmiPrinter.lineWrap(1);
 
-                    await SunmiPrinter.feedPaper();
-                    await SunmiPrinter.cutPaper();
-                   }catch(err){
-                         errorMessage = err.toString();
-      setState(() {});
-                    }
+                        await SunmiPrinter.feedPaper();
+                        await SunmiPrinter.cutPaper();
+                      } catch (err) {
+                        errorMessage = err.toString();
+                        setState(() {});
+                      }
                     },
-
                   ),
-                  
                 ],
               ),
               Row(
@@ -146,79 +140,100 @@ class _MyAppState extends State<MyApp> {
                   ElevatedButton(
                     child: const Text("print Image"),
                     onPressed: () async {
-                      try{
-                    Uint8List bytes = await _getImageFromAsset('assets/print_logo.jpeg');
-                      await SunmiPrinter.printImage(image: bytes, align: SunmiPrintAlign.CENTER);
-                    await SunmiPrinter.lineWrap(1);
-                    await SunmiPrinter.feedPaper();
-                    await SunmiPrinter.cutPaper();
-                      }catch(err){
-                   errorMessage = err.toString();
-                   setState(() {});
+                      try {
+                        Uint8List bytes =
+                            await _getImageFromAsset('assets/print_logo.jpeg');
+                        await SunmiPrinter.printImage(
+                            image: bytes, align: SunmiPrintAlign.CENTER);
+                        await SunmiPrinter.lineWrap(1);
+                        await SunmiPrinter.feedPaper();
+                        await SunmiPrinter.cutPaper();
+                      } catch (err) {
+                        errorMessage = err.toString();
+                        setState(() {});
                       }
                     },
                   ),
-                  
-                         ElevatedButton(
+                  ElevatedButton(
                     child: const Text("print Qr code"),
                     onPressed: () async {
-                     try{
-                await SunmiPrinter.printQr(data: "https://twitter.com/wojoodtech",align:SunmiPrintAlign.CENTER,size: 5);          
-                await SunmiPrinter.lineWrap(1);
-                await SunmiPrinter.feedPaper();
-                await SunmiPrinter.cutPaper();
-                       }catch(err){
-                   errorMessage = err.toString();
-                   setState(() {});
-                      
-                     }
+                      try {
+                        await SunmiPrinter.printQr(
+                            data: "https://twitter.com/wojoodtech",
+                            align: SunmiPrintAlign.CENTER,
+                            size: 5);
+                        await SunmiPrinter.lineWrap(1);
+                        await SunmiPrinter.feedPaper();
+                        await SunmiPrinter.cutPaper();
+                      } catch (err) {
+                        errorMessage = err.toString();
+                        setState(() {});
+                      }
                     },
                   ),
-                
-                  
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                          ElevatedButton(
+                  ElevatedButton(
                     child: const Text("print barcode"),
                     onPressed: () async {
-                     try{
-                await SunmiPrinter.printBarCode(data: "1234567890",height: 50,width: 2,textPosition: SunmiBarcodeTextPos.TEXT_UNDER,barcodeType:SunmiBarcodeType.CODE128,align:SunmiPrintAlign.CENTER );   
-                await SunmiPrinter.lineWrap(1);
-                await SunmiPrinter.feedPaper();
-                await SunmiPrinter.cutPaper();
-                 }catch(err){
-                   errorMessage = err.toString();
-                   setState(() {});
+                      try {
+                        await SunmiPrinter.printBarCode(
+                            data: "1234567890",
+                            height: 50,
+                            width: 2,
+                            textPosition: SunmiBarcodeTextPos.TEXT_UNDER,
+                            barcodeType: SunmiBarcodeType.CODE128,
+                            align: SunmiPrintAlign.CENTER);
+                        await SunmiPrinter.lineWrap(1);
+                        await SunmiPrinter.feedPaper();
+                        await SunmiPrinter.cutPaper();
+                      } catch (err) {
+                        errorMessage = err.toString();
+                        setState(() {});
                       }
                     },
                   ),
-                             ElevatedButton(
+                  ElevatedButton(
                     child: const Text("print table"),
                     onPressed: () async {
-                      try{
-                await SunmiPrinter.printTable(cols: [
-                  ColumnMaker(text:"test#1" ,align:SunmiPrintAlign.LEFT ,width:5),
-                   ColumnMaker(text:"test#2" ,align:SunmiPrintAlign.LEFT ,width: 5),
-                ]);  
-                 await SunmiPrinter.printTable(cols: [
-                  ColumnMaker(text:"test#3" ,align:SunmiPrintAlign.LEFT ,width: 5),
-                   ColumnMaker(text:"test#4" ,align:SunmiPrintAlign.LEFT ,width: 5),
-                ]);  
-                await SunmiPrinter.lineWrap(1);
-                await SunmiPrinter.feedPaper();
-                await SunmiPrinter.cutPaper();
-                       }catch(err){
-                   errorMessage = err.toString();
-                   setState(() {});
+                      try {
+                        await SunmiPrinter.printTable(cols: [
+                          ColumnMaker(
+                              text: "test#1",
+                              align: SunmiPrintAlign.LEFT,
+                              width: 5),
+                          ColumnMaker(
+                              text: "test#2",
+                              align: SunmiPrintAlign.LEFT,
+                              width: 5),
+                        ]);
+                        await SunmiPrinter.printTable(cols: [
+                          ColumnMaker(
+                              text: "test#3",
+                              align: SunmiPrintAlign.LEFT,
+                              width: 5),
+                          ColumnMaker(
+                              text: "test#4",
+                              align: SunmiPrintAlign.LEFT,
+                              width: 5),
+                        ]);
+                        await SunmiPrinter.lineWrap(1);
+                        await SunmiPrinter.feedPaper();
+                        await SunmiPrinter.cutPaper();
+                      } catch (err) {
+                        errorMessage = err.toString();
+                        setState(() {});
                       }
                     },
                   ),
                 ],
               ),
-              const SizedBox(height:15,),
+              const SizedBox(
+                height: 15,
+              ),
               Text("Printer is connect :${SunmiPrinter.isConnected}"),
               Text("Error:$errorMessage")
             ],
